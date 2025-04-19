13842a3becd8f122748580f65c3d63376c3afe82
Viswanath <pingvizcodes@gmail.com>
2025-04-19 10:50:31 -0400

    Initial Commit - all Nextjs files

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function (){
+    return(
+        <p>Customers Page</p>
+    )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Funnel_Sans, Lusitana} from 'next/font/google';
+
+export const funnel = Funnel_Sans({subsets: ['latin']});
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: '400'});
